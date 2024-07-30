88d5573120c7b6b9da55e6cec9afe1b43fd9a4aa
Hamed abdollahzade <hamedabdollahzade.ab@gmail.com>
2024-07-30 12:00:56 +0330

    استفاده از هوک useRouter

--- pages/courses/[id].jsx
+++ pages/courses/[id].jsx
@@ -1,5 +1,10 @@
+import { useRouter } from "next/router";
+
 const CoursesPage = () => {
-  return <div> Dynamic Route</div>;
+  const route = useRouter();
+  console.log("return useRouter in courses page ==> ", route);
+
+  return <h1> Dynamic Route Courses {route.query.id} </h1>;
 };
 
 export default CoursesPage;
