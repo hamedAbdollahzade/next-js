bd7e19334e23bdad0e4936f8ba7506235a123f1f
Hamed abdollahzade <hamedabdollahzade.ab@gmail.com>
2024-07-30 13:50:02 +0330

    route.push

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,13 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const route = useRouter();
+
+  const redirect = () => {
+    route.push("/courses/pwa");
+  };
+
   return (
     <>
       <h1>Home Page </h1>
@@ -9,6 +16,9 @@ export default function Home() {
         <li>
           {/* <a href="/courses">Courses</a>  ==> request to server !! vali ma mikhaym SPA bashe ! pas az Component Link estefade mikonim :)    */}
           <Link href={"/courses"}>courses</Link> {/* SPA Page */}
+        </li>
+        <li>
+          <button onClick={redirect}>Redirect to Custom page </button>
         </li>
       </ul>
     </>
