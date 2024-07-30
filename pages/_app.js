81b7f9e6d4f1a16880848d2a52a9ebfd48417074
Hamed abdollahzade <hamedabdollahzade.ab@gmail.com>
2024-07-30 14:13:49 +0330

    کاربرد فایل app.js_ چیه؟

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
 import "@/styles/globals.css";
 
+// برای نمایش و پیج مد نظر فعلی استفاده میشه
+// هرکامپوننتی ک بخواد تو پیج ما رندر بشه از اینجا گذر میکنند تا نمایش داده شود
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <h1>Header Section</h1>
+      <hr />
+      <Component {...pageProps} />
+      <hr />
+      <h1>Footer Section</h1>
+    </>
+  );
 }
