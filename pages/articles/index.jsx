999c5d26bab00a7b380f93deaf8c95e26ec686b3
Hamed abdollahzade <hamedabdollahzade.ab@gmail.com>
2024-07-31 13:13:39 +0330

    استفاده از روش Css-in-js

--- pages/articles/index.jsx
+++ pages/articles/index.jsx
@@ -1,5 +1,31 @@
 const Articles = () => {
-  return <h1>Articles Page ... </h1>;
+  return (
+    <>
+      {/* Css In Js */}
+      <h1 style={{ color: "blue" }}>Articles Page ... </h1>
+
+      <h2>test Articles 2</h2>
+      {/*  The following libraries are supported in Client Components in the app directory (alphabetical):
+chakra-ui
+kuma-ui
+@mui/material
+@mui/joy
+pandacss
+styled-jsx
+styled-components
+stylex
+tamagui
+tss-react
+vanilla-extract  */}
+
+      {/* styled-jsx : */}
+      <style jsx>{`
+        h2 {
+          color: green;
+        }
+      `}</style>
+    </>
+  );
 };
 
 export default Articles;
